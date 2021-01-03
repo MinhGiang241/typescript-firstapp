8eeb987082961e8073994d983b4671d533f2037b
MinhGiang241 <minhgiang241@gmail.com>
2021-01-03 12:37:18 +0700

    ES modules syntax

---
+++ dist/decorators/autobind.js
@@ -0,0 +1,13 @@
+// autobind decorator
+export function autobind(_, _2, descriptor) {
+    var originalMethod = descriptor.value;
+    var adjDescriptor = {
+        configurable: true,
+        get: function () {
+            var boundFn = originalMethod.bind(this);
+            return boundFn;
+        },
+    };
+    return adjDescriptor;
+}
+//# sourceMappingURL=autobind.js.map
